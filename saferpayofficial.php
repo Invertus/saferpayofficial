7fe73313e2321272691953f6e3aa00ddd0430d45
Alfred Ivasko <alfred@Alfreds-Air.lan>
2022-02-11 10:37:56 +0200

    changelog updated

--- saferpayofficial.php
+++ saferpayofficial.php
@@ -41,7 +41,7 @@ class SaferPayOfficial extends PaymentModule
     {
         $this->name = 'saferpayofficial';
         $this->author = 'Invertus';
-        $this->version = '1.0.13';
+        $this->version = '1.0.14';
         $this->module_key = '3d3506c3e184a1fe63b936b82bda1bdf';
         $this->displayName = 'SaferpayOfficial';
         $this->description = 'Saferpay Payment module';
