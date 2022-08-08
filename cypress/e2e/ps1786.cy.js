c23528c62bee3338c9bcfbba8a099d987cc6d50d
SimonasB88 <simonas.burneikis@invertus.eu>
2022-08-08 10:05:04 +0300

    Update ps1786.cy.js

--- cypress/e2e/ps1786.cy.js
+++ cypress/e2e/ps1786.cy.js
@@ -52,7 +52,7 @@ describe('PS1786 Saferpay Tests Suite', () => {
       cy.viewport(1920,1080)
       login('SaferpayBOFOLoggingIn')
   })
-it('01 Connecting the Test API information to module', () => {
+it.only('01 Connecting the Test API information to module', () => {
       cy.visit('https://sp1786.eu.ngrok.io/admin1/')
       cy.get('#subtab-AdminParentModulesSf > :nth-child(1)').click()
       cy.get('.pstaggerAddTagInput').type('saferpay')
@@ -67,13 +67,13 @@ it('01 Connecting the Test API information to module', () => {
       cy.get('#configuration_fieldset_1 > .panel-footer > .btn').click()
       cy.get(':nth-child(4) > .alert').should('exist')
 })
-it('02 Enabling Saferpay carriers and countries successfully', () => {
+it.only('02 Enabling Saferpay carriers and countries successfully', () => {
       cy.visit('https://sp1786.eu.ngrok.io/admin1/')
       cy.get('[id="subtab-AdminPaymentPreferences"]').find('[href]').eq(0).click({force:true})
       cy.get('[class="js-multiple-choice-table-select-column"]').eq(7).click()
       cy.get('[class="btn btn-primary"]').eq(3).click()
 })
-it('03 Enabling All payments in Module BO', () => {
+it.only('03 Enabling All payments in Module BO', () => {
       cy.visit('https://sp1786.eu.ngrok.io/admin1/')
       cy.get('#subtab-AdminParentModulesSf > :nth-child(1)').click()
       cy.get('.pstaggerAddTagInput').type('saferpay')
@@ -574,7 +574,7 @@ it('33 KLARNA BO Order Refunding', () => {
       cy.get('[class="alert alert-success d-print-none"]').should('be.visible') //visible success message
       cy.contains('Order Refunded by Saferpay').should('be.visible')
 })
-it.only('34 GOOGLEPAY Checkouting', () => {
+it('34 GOOGLEPAY Checkouting', () => { //TODO to finish
       cy.visit('https://sp1786.eu.ngrok.io/en/index.php?controller=history')
       cy.get('a').click()
       cy.contains('Reorder').click()
