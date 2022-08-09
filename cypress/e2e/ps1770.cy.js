68baa7a43986d2afb3e8c158f15b00ee2a096797
SimonasB88 <simonas.burneikis@invertus.eu>
2022-08-09 17:10:32 +0300

    test updates

--- cypress/e2e/ps1770.cy.js
+++ cypress/e2e/ps1770.cy.js
@@ -52,45 +52,45 @@ describe('PS1770 Saferpay Tests Suite', () => {
       cy.viewport(1920,1080)
       login('SaferpayBOFOLoggingIn')
   })
-// it('01 Connecting the Test API information to module', () => {
-//       cy.visit('https://sp1770.eu.ngrok.io/admin1/')
-//       cy.get('#subtab-AdminParentModulesSf > :nth-child(1)').click()
-//       cy.get('.pstaggerAddTagInput').type('saferpay')
-//       cy.get('#module-search-button').click()
-//       cy.get('.btn-group > .btn-primary-reverse').click()  //clicking the Congifure
-//       cy.get('[name="SAFERPAY_USERNAME_TEST"]').type((Cypress.env('SAFERPAY_USERNAME_TEST')),{delay: 0, log: false})
-//       cy.get('[name="SAFERPAY_PASSWORD_TEST"]').type((Cypress.env('SAFERPAY_PASSWORD_TEST')),{delay: 0, log: false})
-//       cy.get('[name="SAFERPAY_CUSTOMER_ID_TEST"]').type((Cypress.env('SAFERPAY_CUSTOMER_ID_TEST')),{delay: 0, log: false})
-//       cy.get('[name="SAFERPAY_TERMINAL_ID_TEST"]').type((Cypress.env('SAFERPAY_TERMINAL_ID_TEST')),{delay: 0, log: false})
-//       cy.get('[name="SAFERPAY_MERCHANT_EMAILS_TEST"]').type((Cypress.env('SAFERPAY_MERCHANT_EMAILS_TEST')),{delay: 0, log: false})
-//       cy.get('[name="SAFERPAY_FIELDS_ACCESS_TOKEN_TEST"]').type((Cypress.env('SAFERPAY_FIELDS_ACCESS_TOKEN_TEST')),{delay: 0, log: false})
-//       cy.get('#configuration_fieldset_1 > .panel-footer > .btn').click()
-//       cy.get(':nth-child(4) > .alert').should('exist')
-// })
-// it('02 Enabling Saferpay carriers and countries successfully', () => {
-//       cy.visit('https://sp1770.eu.ngrok.io/admin1/')
-//       cy.get('[id="subtab-AdminPaymentPreferences"]').find('[href]').eq(0).click({force:true})
-//       cy.get('[class="js-multiple-choice-table-select-column"]').eq(7).click()
-//       cy.get('[class="btn btn-primary"]').eq(3).click()
-// })
-// it('03 Enabling All payments in Module BO', () => {
-//       cy.visit('https://sp1770.eu.ngrok.io/admin1/')
-//       cy.get('#subtab-AdminParentModulesSf > :nth-child(1)').click()
-//       cy.get('.pstaggerAddTagInput').type('saferpay')
-//       cy.get('#module-search-button').click()
-//       cy.get('.btn-group > .btn-primary-reverse').click()  //clicking the Congifure
-//       cy.get('#subtab-AdminSaferPayOfficialPayment').click()
-//       //todo update selectors
-//       cy.get('.saferpay-group.all-payments > .col-lg-9 > .form-group > :nth-child(1) > .checkbox > .container-checkbox > .checkmark').click()
-//       cy.get('.saferpay-group.all-payments > .col-lg-9 > .form-group > :nth-child(2) > .checkbox > .container-checkbox > .checkmark').click()
-//       cy.get('.saferpay-group.all-payments > .col-lg-9 > .form-group > :nth-child(3) > .checkbox > .container-checkbox > .checkmark').click()
-//       cy.get('#all_countries_chosen > .chosen-choices > .search-field > .default').click()
-//       cy.get('.highlighted').click()
-//       cy.get('#all_currencies_chosen > .chosen-choices > .search-field > .default').click()
-//       cy.get('.highlighted').click()
-//       cy.get('#configuration_form_submit_btn').click()
-//       cy.get('[class="alert alert-success"]').should('be.visible')
-// })
+it('01 Connecting the Test API information to module', () => {
+      cy.visit('https://sp1770.eu.ngrok.io/admin1/')
+      cy.get('#subtab-AdminParentModulesSf > :nth-child(1)').click()
+      cy.get('.pstaggerAddTagInput').type('saferpay')
+      cy.get('#module-search-button').click()
+      cy.get('.btn-group > .btn-primary-reverse').click()  //clicking the Congifure
+      cy.get('[name="SAFERPAY_USERNAME_TEST"]').type((Cypress.env('SAFERPAY_USERNAME_TEST')),{delay: 0, log: false})
+      cy.get('[name="SAFERPAY_PASSWORD_TEST"]').type((Cypress.env('SAFERPAY_PASSWORD_TEST')),{delay: 0, log: false})
+      cy.get('[name="SAFERPAY_CUSTOMER_ID_TEST"]').type((Cypress.env('SAFERPAY_CUSTOMER_ID_TEST')),{delay: 0, log: false})
+      cy.get('[name="SAFERPAY_TERMINAL_ID_TEST"]').type((Cypress.env('SAFERPAY_TERMINAL_ID_TEST')),{delay: 0, log: false})
+      cy.get('[name="SAFERPAY_MERCHANT_EMAILS_TEST"]').type((Cypress.env('SAFERPAY_MERCHANT_EMAILS_TEST')),{delay: 0, log: false})
+      cy.get('[name="SAFERPAY_FIELDS_ACCESS_TOKEN_TEST"]').type((Cypress.env('SAFERPAY_FIELDS_ACCESS_TOKEN_TEST')),{delay: 0, log: false})
+      cy.get('#configuration_fieldset_1 > .panel-footer > .btn').click()
+      cy.get(':nth-child(4) > .alert').should('exist')
+})
+it('02 Enabling Saferpay carriers and countries successfully', () => {
+      cy.visit('https://sp1770.eu.ngrok.io/admin1/')
+      cy.get('[id="subtab-AdminPaymentPreferences"]').find('[href]').eq(0).click({force:true})
+      cy.get('[class="js-multiple-choice-table-select-column"]').eq(7).click()
+      cy.get('[class="btn btn-primary"]').eq(3).click()
+})
+it('03 Enabling All payments in Module BO', () => {
+      cy.visit('https://sp1770.eu.ngrok.io/admin1/')
+      cy.get('#subtab-AdminParentModulesSf > :nth-child(1)').click()
+      cy.get('.pstaggerAddTagInput').type('saferpay')
+      cy.get('#module-search-button').click()
+      cy.get('.btn-group > .btn-primary-reverse').click()  //clicking the Congifure
+      cy.get('#subtab-AdminSaferPayOfficialPayment').click()
+      //todo update selectors
+      cy.get('.saferpay-group.all-payments > .col-lg-9 > .form-group > :nth-child(1) > .checkbox > .container-checkbox > .checkmark').click()
+      cy.get('.saferpay-group.all-payments > .col-lg-9 > .form-group > :nth-child(2) > .checkbox > .container-checkbox > .checkmark').click()
+      cy.get('.saferpay-group.all-payments > .col-lg-9 > .form-group > :nth-child(3) > .checkbox > .container-checkbox > .checkmark').click()
+      cy.get('#all_countries_chosen > .chosen-choices > .search-field > .default').click()
+      cy.get('.highlighted').click()
+      cy.get('#all_currencies_chosen > .chosen-choices > .search-field > .default').click()
+      cy.get('.highlighted').click()
+      cy.get('#configuration_form_submit_btn').click()
+      cy.get('[class="alert alert-success"]').should('be.visible')
+})
 it.only('04 TWINT Checkouting', () => {
       cy.visit('https://sp1770.eu.ngrok.io/index.php?controller=history')
       cy.contains('Reorder').click()
@@ -111,7 +111,7 @@ it.only('04 TWINT Checkouting', () => {
       })
       cy.get('[id="content-hook_order_confirmation"]').should('exist') //verification of Success Screen
       })
-it('05 TWINT BO Order Refunding', () => {
+it.only('05 TWINT BO Order Refunding', () => {
       cy.visit('https://sp1770.eu.ngrok.io/admin1/index.php?controller=AdminOrders')
       cy.get(':nth-child(1) > .column-payment').click()
       cy.contains('Payment completed by Saferpay').should('be.visible')
@@ -121,11 +121,10 @@ it('05 TWINT BO Order Refunding', () => {
       cy.get('[class="alert alert-success d-print-none"]').should('be.visible') //visible success message
       cy.contains('Order Refunded by Saferpay').should('be.visible')
 })
-it('06 LASTSCHRIFT Checkouting', () => {
+it.only('06 LASTSCHRIFT Checkouting', () => {
       cy.visit('https://sp1770.eu.ngrok.io/index.php?controller=history')
-      cy.get('a').click()
       cy.contains('Reorder').click()
-      cy.get('#id-address-delivery-address-8').click()
+      cy.get('#id-address-delivery-address-7').click()
       //Billing country LT, DE etc.
       cy.get('.clearfix > .btn').click()
       cy.get('#js-delivery > .continue').click()
@@ -142,7 +141,7 @@ it('06 LASTSCHRIFT Checkouting', () => {
       })
       cy.get('[id="content-hook_order_confirmation"]').should('exist') //verification of Success Screen
       })
-it('07 LASTSCHRIFT BO Order Refunding', () => {
+it.only('07 LASTSCHRIFT BO Order Refunding', () => {
       cy.visit('https://sp1770.eu.ngrok.io/admin1/index.php?controller=AdminOrders')
       cy.get(':nth-child(1) > .column-payment').click()
       cy.contains('Payment completed by Saferpay').should('be.visible')
@@ -152,9 +151,8 @@ it('07 LASTSCHRIFT BO Order Refunding', () => {
       cy.get('[class="alert alert-success d-print-none"]').should('be.visible') //visible success message
       cy.contains('Order Refunded by Saferpay').should('be.visible')
 })
-it('08 VISA Checkouting', () => {
+it.only('08 VISA Checkouting', () => {
       cy.visit('https://sp1770.eu.ngrok.io/index.php?controller=history')
-      cy.get('a').click()
       cy.contains('Reorder').click()
       cy.get('#id-address-delivery-address-8').click()
       //Billing country LT, DE etc.
@@ -173,7 +171,7 @@ it('08 VISA Checkouting', () => {
       })
       cy.get('[id="content-hook_order_confirmation"]').should('exist') //verification of Success Screen
       })
-it('09 VISA BO Order Refunding', () => {
+it.only('09 VISA BO Order Refunding', () => {
       cy.visit('https://sp1770.eu.ngrok.io/admin1/index.php?controller=AdminOrders')
       cy.get(':nth-child(1) > .column-payment').click()
       cy.contains('Payment completed by Saferpay').should('be.visible')
