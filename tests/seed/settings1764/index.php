d5b4988fe0eb5df5415390c9ed052200f225dd2a
Mantas Šmulkštys <mantas.smulkstys@invertus.eu>
2023-11-23 10:02:17 +0200

    fix index.php formatting

--- tests/seed/settings1764/index.php
+++ tests/seed/settings1764/index.php
@@ -20,8 +20,6 @@
  *@copyright SIX Payment Services
  *@license   SIX Payment Services
  */
-
-
 header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
 header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
 
