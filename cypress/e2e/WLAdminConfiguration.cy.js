445a84f581c7bf6febe32b68496927dd9fa227ab
SimonasB88 <simonas.burneikis@invertus.eu>
2024-01-09 14:31:53 +0200

    Update WLAdminConfiguration.cy.js

--- cypress/e2e/WLAdminConfiguration.cy.js
+++ cypress/e2e/WLAdminConfiguration.cy.js
@@ -1,5 +1,9 @@
 describe('Admin journey - part 1', () => {
   it('1 - Configure plugin credentials', () => {
-    cy.visit('/admin1')
+    cy.viewport(1920,1080)
+    cy.visit('/admin1/')
+    cy.get('#email').type('demo@demo.com')
+    cy.get('#passwd').type('demodemo')
+    cy.get('#submit_login').click()
   })
 })
