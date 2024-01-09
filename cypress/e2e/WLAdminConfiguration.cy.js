9f77dbc3d3bddb237c4dc1448a02e0d6b8e09887
SimonasB88 <simonas.burneikis@invertus.eu>
2024-01-09 15:40:10 +0200

    Update WLAdminConfiguration.cy.js

--- cypress/e2e/WLAdminConfiguration.cy.js
+++ cypress/e2e/WLAdminConfiguration.cy.js
@@ -2,8 +2,8 @@ describe('Admin journey - part 1', () => {
   it('1 - Configure plugin credentials', () => {
     cy.viewport(1920,1080)
     cy.visit('/admin1/')
-    cy.get('#email').type('demo@demo.com')
-    cy.get('#passwd').type('demodemo')
+    cy.get('#email').type('demo@prestashop.com')
+    cy.get('#passwd').type('prestashop_demo')
     cy.get('#submit_login').click()
   })
 })
