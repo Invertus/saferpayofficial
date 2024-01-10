4c183e51e5ebb9c7f5065d5170e5d79441400777
SimonasB88 <simonas.burneikis@invertus.eu>
2024-01-10 15:55:52 +0200

    test code updates

--- cypress/e2e/WLCustomerJourney.cy.js
+++ cypress/e2e/WLCustomerJourney.cy.js
@@ -13,9 +13,18 @@ describe('Admin journey - part 1', () => {
     cy.contains('LT') // choosing carriers
     cy.get('.clearfix > .btn').click()
     cy.get('#js-delivery > .continue').click()
-    cy.contains('Pay with Worldline Online Payments').click()
+    // validation of the PMs
+    cy.contains('Pay with Worldline Online Payments').should('exist').should('be.visible')
+    cy.contains('Pay with MasterCard').should('exist').should('be.visible')
+    cy.contains('Pay with VISA').should('exist').should('be.visible')
+    // missing PM selection in the scenario, temporary taking MasterCard
+    cy.contains('Pay with MasterCard').click()
     cy.get('.condition-label > .js-terms').click()
     cy.contains('Place order').click()
+    cy.get('#payment-cardnumber').type('5137009801943438')
+    cy.get('#payment-cardholdername').type('MR TEST')
+    cy.get('#payment-cvc').type('123')
+    cy.get('#submit-all').click()
   })
   it('2 - Worldline environment', () => {
     // todo
