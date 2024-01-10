aa3bc6fae0f27befd7cad8f294dcc773a373bb97
SimonasB88 <simonas.burneikis@invertus.eu>
2024-01-10 17:33:10 +0200

    test code updates

--- cypress/e2e/WLCustomerJourney02.cy.js
+++ cypress/e2e/WLCustomerJourney02.cy.js
@@ -1,9 +1,21 @@
 describe('Admin journey - part 2', () => {
-  it.only('1 - Check order', () => {
+  it('1 - Check order', () => {
     cy.LogInBO()
     cy.get('#subtab-AdminOrders > .link').click({force:true})
+    cy.get(':nth-child(1) > .column-reference')
+    cy.get(':nth-child(1) > .choice-type').contains('Awaiting payment capture').should('be.visible').should('exist')
+    cy.get(':nth-child(1) > .order_price-type').contains('€115.20').should('be.visible').should('exist') // hardcoded price verifying
+    cy.get(':nth-child(1) > .column-payment').click() // navigating into the last Order
+    cy.get('#worldlineop-admin-order').scrollIntoView()
+    cy.get('[id="worldlineop-admin-order"]').contains('115.20 EUR').should('be.visible').should('exist') // hardcoded price verifying
+    cy.get(':nth-child(1) > .col-md-12 > .info-block > .row > :nth-child(2) > :nth-child(2)') // todo transaction ID
   })
   it('2 - Fully capture transaction', () => {
-    // todo
+    cy.LogInBO()
+    cy.get('#subtab-AdminOrders > .link').click({force:true})
+    cy.get(':nth-child(1) > .column-reference')
+    cy.get(':nth-child(1) > .column-payment').click() // navigating into the last Order
+    cy.get('#worldlineop-admin-order').scrollIntoView()
+    cy.get('#worldlineop-btn-capture').click()
   })
 })
