42c4961a3cbc7bdeeceeb468aab25c3a77ef6c5b
SimonasB88 <simonas.burneikis@invertus.eu>
2024-01-10 17:50:47 +0200

    test code updates

--- cypress/e2e/WLCustomerJourney02.cy.js
+++ cypress/e2e/WLCustomerJourney02.cy.js
@@ -18,5 +18,9 @@ describe('Admin journey - part 2', () => {
     cy.get('#worldlineop-admin-order').scrollIntoView()
     cy.get('#worldlineop-btn-capture').click()
     cy.contains('Capture requested successfully').should('exist').should('be.visible')
+    cy.get(':nth-child(1) > .card > .card-body > :nth-child(1) > .col > :nth-child(2) > :nth-child(2)').contains('0.00 EUR') // hardcoded so far - Amount captured
+    cy.get(':nth-child(1) > .card > .card-body > :nth-child(1) > .col > :nth-child(3) > :nth-child(2)').contains('115.20 EUR') // hardcoded so far - Amount pending capture
+    cy.get(':nth-child(1) > .card > .card-body > :nth-child(1) > .col > :nth-child(4) > :nth-child(2)').contains('0.00 EUR') // hardcoded so far - Amount that can be captured
+    cy.get('#update_order_status_action_input').contains('Awaiting payment capture').should('be.visible').should('exist')
   })
 })
