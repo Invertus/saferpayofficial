b503cb310fd92105416e8cb73c8e7ed81aba7223
Justas Vaitkus <j.vaitkus@Justass-MacBook-Air.local>
2024-08-07 11:50:27 +0300

    seed update

--- tests/seed/settings1786/parameters.php
+++ tests/seed/settings1786/parameters.php
@@ -1,31 +1,8 @@
-<?php
-/**
- *NOTICE OF LICENSE
- *
- *This source file is subject to the Open Software License (OSL 3.0)
- *that is bundled with this package in the file LICENSE.txt.
- *It is also available through the world-wide-web at this URL:
- *http://opensource.org/licenses/osl-3.0.php
- *If you did not receive a copy of the license and are unable to
- *obtain it through the world-wide-web, please send an email
- *to license@prestashop.com so we can send you a copy immediately.
- *
- *DISCLAIMER
- *
- * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
- *versions in the future. If you wish to customize PrestaShop for your
- *needs please refer to http://www.prestashop.com for more information.
- *
- *@author INVERTUS UAB www.invertus.eu  <support@invertus.eu>
- *@copyright SIX Payment Services
- *@license   SIX Payment Services
- */
-
-
-return [
-  'parameters' => [
+<?php return array (
+  'parameters' => 
+  array (
     'database_host' => 'mysql',
-    'database_port' => '',
+    'database_port' => '3306',
     'database_name' => 'prestashop',
     'database_user' => 'root',
     'database_password' => 'prestashop',
@@ -33,16 +10,55 @@ return [
     'database_engine' => 'InnoDB',
     'mailer_transport' => 'smtp',
     'mailer_host' => '127.0.0.1',
-    'mailer_user' => null,
-    'mailer_password' => null,
-    'secret' => '5me4mdumTjYo4jPpNtQSuRN1MyD9wSSze7VuLg4s2ktMUxkXHihrLXZV1NaHuzhU',
+    'mailer_user' => NULL,
+    'mailer_password' => NULL,
+    'secret' => 'cbay7ahbCdIWniHDsIJYCZ14MKABp4e0w7UUaFFRtcAFhy7e6RefDkMLiPS3vjfR',
     'ps_caching' => 'CacheMemcache',
     'ps_cache_enable' => false,
-    'ps_creation_date' => '2020-12-23',
+    'ps_creation_date' => '2024-08-06',
     'locale' => 'en-US',
     'use_debug_toolbar' => true,
-    'cookie_key' => '6NiLzzVp8Z9LiOtHgBNScv929I2K8a2A8jNppJVz1t5WjnnRGPbXVXKS',
-    'cookie_iv' => 'TtPM0O7S',
-    'new_cookie_key' => 'def000001b90465d16f2aa760abe76e1a88fb80071075978db96754d16fd83cef2c9d8d1218a6dba2f28cee48e527700e85ddf87731652776449d60f9f4e1cceb90198b2',
-  ],
-];
+    'cookie_key' => 'moFmacgxEW7GyAU1myGgTL9vrOP6Vmxq6QaMXTGi94TzzcPaQJUGCb55VOkFnaXW',
+    'cookie_iv' => 'z252pKF1u6Egh1PEnvC2337sz3nTpBFB',
+    'new_cookie_key' => 'def00000c45cd3f4771eae1c2f531dd9293eb1f7be5f4e889839ceb339c1fda8a5f2bf190243f21f4d05f60009b59f93b7e319781e55752b40d2f5d865fb684b7201a648',
+    'api_public_key' => '-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEArUboxQdXYPmw/Vp/8QPG
+KAzd5biPZHOfTdnQB98ZZgkjgZT2IUBiY3bQ/IPGn9otmc67ZqPqPOS0Ouyp8GZf
+Tpp683dh7V0ax/dCy8fIWKQJFVOXjaj0V6mtypTDNPNASCqWm4fYEj5XIILDkEXp
+LlxSBnel61YzEkOMtg5uv1qF8rAsT9u2xR4/Wgvnh+QR3ir6x6z+LLcYGB34bERU
+oQrybmruEXi/h8A+mebHP/qOdGCq3naoOXcMGF36LXTbBSFTRT42bdDGcV83EsGm
+N5LQZ5t4dIac/Kkw1vwpoLicDC9WW9vKrXeAKc6SxAiKglpQecJjBeF42rUv5OO3
+7QIDAQAB
+-----END PUBLIC KEY-----
+',
+    'api_private_key' => '-----BEGIN PRIVATE KEY-----
+MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCtRujFB1dg+bD9
+Wn/xA8YoDN3luI9kc59N2dAH3xlmCSOBlPYhQGJjdtD8g8af2i2Zzrtmo+o85LQ6
+7KnwZl9Omnrzd2HtXRrH90LLx8hYpAkVU5eNqPRXqa3KlMM080BIKpabh9gSPlcg
+gsOQRekuXFIGd6XrVjMSQ4y2Dm6/WoXysCxP27bFHj9aC+eH5BHeKvrHrP4stxgY
+HfhsRFShCvJuau4ReL+HwD6Z5sc/+o50YKredqg5dwwYXfotdNsFIVNFPjZt0MZx
+XzcSwaY3ktBnm3h0hpz8qTDW/CmguJwML1Zb28qtd4ApzpLECIqCWlB5wmMF4Xja
+tS/k47ftAgMBAAECggEAApeW/kcwDSNP3lb48DI+eLVWePCl/XRmlS/rkP2gb//g
+eaYkePBY5Wd/moZPkRm3MhzRcZYvosHOWakfaBMu1od/5PAtSR4OnND1tzTYo+vH
+txn7oGXp0INWPFoXIHJlIiSTbxyJM+HOuGMYlzximPB/bhxu6mTCdpcsJwT5ifcG
+ul8BVOFIJE7yIHErvvZKAl3A/MQ+v93L/eveuiDWLaxWZ95nwg0yEnkX00ooDxIR
+VaZp2BMSN0sSsMe43V0tE2B6n87mX6tn9i5mmRQTlAIg37Yn128O4N3Ala7X4LqR
+sipriSek5rOa69prLRfjX1/G081Yz44E448pegzThwKBgQDru9aNQmV8N1GEG5UT
+PT+m18BS/7QQ1S0Sg5PNcYwp/pcrqRSD4oRbauRqxSF+tBFqGais2PBKd9zEy/Hb
+Qibht0enQeXCXdtFQGzrO5HgS8uyev/Hg5P6yGw82qnuwfRZkLOwHX+5E88m/27Q
++qRp4ny8Q5hlhb0eYWZLX3/dswKBgQC8LHzNt3zQKApz5SpXZLOFRS8MEDD1Ely6
+7lGvNeU6gqkGDNofCV9HocFIlLVje1JVqTZ3r7oHXkcqtRDdv4HVbI30Ouau8D24
+CLIhs1ZFlbxUeJV3ao4MC2BkBYXKc/1wY9ceQCpUfhoYPTqKSkEI6ZkmrWyIdxQU
+VMaeIDWD3wKBgQCazzW8ePEq3S1UY8TH6L5HdILiPvji4QyDjxNepQYWghKG4rgN
+UJH5cz5ke+deTSTul8E1Ghbb4NGEECdaBZyBORr+VYFs3rRkS0X8SZeoRD6WjL4z
+KU7iMAMdB8D3gjpVI3lN3GGHiuHMy4zcAu48dzfRb1bCvUyQighi0WmojwKBgDdJ
+PtXWHiauTPvmDb2liZUTUZJSw3+NiI8na/p1dEbvkqeDaSaLQ2IxZS9k+bIjznn6
+gFVD3P1DNteWYUE1ffjtrvtdvt8RGKEUdPmvzh4dszqCPy/Zr6hcQ1K/yKJF1+WI
+f/CUW5t9fT835hYxYNA0idmyRFwH2G9sxPo6bl6zAoGARA+q9wKDR2Adi1j+rFxx
+Gkn0xEMHX2D6tXMzU7vRAosdDTNO9uVjDFt03YIsWTQO2aHWdSQAdBTSmXIT+o5k
+vm124LvaxdrQaCbtpFxk0o9iwJY2W4vY4sN5RD9DIrsYgKJ7vOgciuJjrJ2AtI54
+gY15vMvW/GzGng/yLi8ncSg=
+-----END PRIVATE KEY-----
+',
+  ),
+);
