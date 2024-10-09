3631a0b68675771eec1315a066be5c906dc4bfb0
MarijusCoding <marijusdilys9@gmail.com>
2024-10-09 09:21:26 +0300

    whitespace

--- src/Service/SaferPayRefreshPaymentsService.php
+++ src/Service/SaferPayRefreshPaymentsService.php
@@ -61,7 +61,6 @@ class SaferPayRefreshPaymentsService
 
     public function refreshPayments()
     {
-
         // Get enabled payments.
         $activePayments = $this->paymentRepository->getActivePaymentMethods();
 
