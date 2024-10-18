3ad9df9e035c96f81e728b720d34c647b2fa541c
MarijusCoding <marijusdilys9@gmail.com>
2024-10-18 15:58:06 +0300

    removed config

--- src/Config/SaferPayConfig.php
+++ src/Config/SaferPayConfig.php
@@ -278,7 +278,6 @@ class SaferPayConfig
     const THE_CHECKOUT_MODULE = 'thecheckout';
     const SUPER_CHECKOUT_MODULE = 'supercheckout';
     const THE_CHECKOUT_FRONT_CONTROLLER = 'TheCheckoutModuleFrontController';
-    const SUPER_CHECKOUT_FRONT_CONTROLLER = 'SupercheckoutSupercheckoutModuleFrontController';
     const OPC_MODULE_LIST = [
         self::ONE_PAGE_CHECKOUT_MODULE,
         self::THE_CHECKOUT_MODULE,
