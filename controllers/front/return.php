62a6c4d5bd9e1e2ad27f2b65247e158f89a474e6
MarijusCoding <marijusdilys9@gmail.com>
2025-01-16 17:39:20 +0200

    [SL-282] removed order creation on return

--- controllers/front/return.php
+++ controllers/front/return.php
@@ -55,7 +55,6 @@ class SaferPayOfficialReturnModuleFrontController extends AbstractSaferPayContro
         $cartId = (int) Tools::getValue('cartId');
         $order = new Order($this->getOrderId($cartId));
         $secureKey = Tools::getValue('secureKey');
-        $selectedCard = Tools::getValue('selectedCard');
         $paymentMethod = $order->id ? $order->payment : Tools::getValue('paymentMethod');
         $cart = new Cart($cartId);
         $failController = $this->getFailController($paymentMethod);
@@ -70,74 +69,9 @@ class SaferPayOfficialReturnModuleFrontController extends AbstractSaferPayContro
             $this->redirectWithNotifications($this->getRedirectionToControllerUrl($failController));
         }
 
-        /** @var SaferPayTransactionAssertion $transactionAssert */
-        $transactionAssert = $this->module->getService(SaferPayTransactionAssertion::class);
-
-        try {
-            $assertResponseBody = $transactionAssert->assert(
-                $cartId,
-                (int) $selectedCard === SaferPayConfig::CREDIT_CARD_OPTION_SAVE,
-                $selectedCard,
-                (int) Tools::getValue(SaferPayConfig::IS_BUSINESS_LICENCE)
-            );
-            $transactionStatus = $assertResponseBody->getTransaction()->getStatus();
-        } catch (Exception $e) {
-            $logger->error($e->getMessage(), [
-                'context' => [],
-                'exceptions' => ExceptionUtility::getExceptions($e),
-            ]);
-
-            $this->warning[] = $this->module->l('An error occurred. Please contact support', self::FILE_NAME);
-            $this->redirectWithNotifications($this->getRedirectionToControllerUrl($failController));
-        }
-
-        $orderPayment = $assertResponseBody->getPaymentMeans()->getBrand()->getPaymentMethod();
-
-        if (!empty($assertResponseBody->getPaymentMeans()->getWallet())) {
-            $orderPayment = $assertResponseBody->getPaymentMeans()->getWallet();
-        }
-
-        /** @var SaferPayFieldRepository $saferPayFieldRepository */
-        $saferPayFieldRepository = $this->module->getService(SaferPayFieldRepository::class);
-
-        /**
-         * NOTE: This flow is for hosted iframe payment method
-         */
-        if (Configuration::get(SaferPayConfig::BUSINESS_LICENSE . SaferPayConfig::getConfigSuffix())
-            || Configuration::get(SaferPayConfig::FIELDS_ACCESS_TOKEN . SaferPayConfig::getConfigSuffix())
-            || $saferPayFieldRepository->isActiveByName($orderPayment))
-        {
-            $order = new Order($this->getOrderId($cartId));
-
-            try {
-                $this->createAndValidateOrder($assertResponseBody, $transactionStatus, $cartId, $orderPayment);
-            } catch (Exception $e) {
-                $logger->debug($e->getMessage(), [
-                    'context' => [],
-                    'exceptions' => ExceptionUtility::getExceptions($e),
-                ]);
-
-                $this->warning[] = $this->module->l('An error occurred. Please contact support', self::FILE_NAME);
-                $this->redirectWithNotifications($this->getRedirectionToControllerUrl('fail'));
-            }
-        }
-
-        try {
-            /** @var SaferPayOrderStatusService $orderStatusService */
-            $orderStatusService = $this->module->getService(SaferPayOrderStatusService::class);
-            if ($assertResponseBody->getTransaction()->getStatus() === TransactionStatus::PENDING) {
-                $orderStatusService->setPending($order);
-            }
-        } catch (SaferPayApiException $e) {
-            $logger->debug($e->getMessage(), [
-                'context' => [],
-                'exceptions' => ExceptionUtility::getExceptions($e),
-            ]);
-            // we only care if we have a response with pending status, else we skip further actions
-        }
-
         $logger->debug(sprintf('%s - Controller action ended', self::FILE_NAME));
     }
+
     /**
      * @throws PrestaShopException
      */
