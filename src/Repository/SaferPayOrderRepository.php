41e2d8b9c29d9c8c389f83bee4450813ed7e2df3
MarijusCoding <marijusdilys9@gmail.com>
2025-03-26 10:36:03 +0200

    [SL-298] initialize fix

--- src/Repository/SaferPayOrderRepository.php
+++ src/Repository/SaferPayOrderRepository.php
@@ -60,6 +60,7 @@ class SaferPayOrderRepository
         $query->select('`id_saferpay_order`');
         $query->from('saferpay_order');
         $query->where('id_cart = "' . (int) $cartId . '"');
+        $query->orderBy('`id_saferpay_order` DESC');
 
         return Db::getInstance()->getValue($query);
     }
