46af85370b18710f385261f80a29a0659d164e71
MarijusCoding <marijusdilys9@gmail.com>
2025-03-26 10:39:08 +0200

    [SL-298] fix other repo method

--- src/Repository/SaferPayOrderRepository.php
+++ src/Repository/SaferPayOrderRepository.php
@@ -50,6 +50,7 @@ class SaferPayOrderRepository
         $query->select('`id_saferpay_order`');
         $query->from('saferpay_order');
         $query->where('id_order = "' . (int) $orderId . '"');
+        $query->orderBy('`id_saferpay_order` DESC');
 
         return Db::getInstance()->getValue($query);
     }
