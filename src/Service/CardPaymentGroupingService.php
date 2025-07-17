36e34d0ed77761b1007110fcde86dc03d8bcdb15
MarijusDilys <marijus.dilys@invertus.eu>
2025-07-17 15:05:14 +0300

    [SL-312] fix

--- src/Service/CardPaymentGroupingService.php
+++ src/Service/CardPaymentGroupingService.php
@@ -40,18 +40,23 @@ class CardPaymentGroupingService
     public function group(array $paymentMethods, array $allCurrencies): array
     {
         $result = [];
+        $cardMethods = [];
 
         foreach ($paymentMethods as $method) {
-            if (!in_array($method['paymentMethod'], SaferPayConfig::CARD_BRANDS, true)) {
+            if (in_array($method, SaferPayConfig::CARD_BRANDS, true)) {
+                $cardMethods[] = $method;
+            } else {
                 $result[] = $method;
             }
         }
 
-        $result[] = [
-            'paymentMethod' => SaferPayConfig::PAYMENT_CARDS,
-            'logoUrl' => '',
-            'currencies' => $allCurrencies,
-        ];
+        if (!empty($cardMethods)) {
+            $result[] = [
+                'paymentMethod' => SaferPayConfig::PAYMENT_CARDS,
+                'logoUrl' => '',
+                'currencies' => $allCurrencies,
+            ];
+        }
 
         return $result;
     }
