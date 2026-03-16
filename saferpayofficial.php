6b92b9f7b4fdd79a151920600d3568406896265c
Gytautas Zumaras <gytautaszumaras@Gytautas-Zumaras.local>
2026-03-16 17:15:10 +0200

    remove upgrade

--- saferpayofficial.php
+++ saferpayofficial.php
@@ -65,7 +65,7 @@ class SaferPayOfficial extends PaymentModule
     {
         $this->name = 'saferpayofficial';
         $this->author = 'Invertus';
-        $this->version = '2.0.3';
+        $this->version = '2.0.2';
         $this->module_key = '3d3506c3e184a1fe63b936b82bda1bdf';
         $this->displayName = 'SaferpayOfficial';
         $this->description = 'Saferpay Payment module';
