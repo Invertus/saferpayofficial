Invertus/saferpayofficial

commit f7adabca40e6f3c4d8ce02428b559d7ef2ad8c5d

Escape Smarty variables in templates to satisfy PS validator
- options.tpl: add |escape:'htmlall':'UTF-8' to id, class, key and value
  outputs inside the password_input block.
- saferpay_wait.tpl: move checkStatusEndpoint to a data attribute escaped
  with htmlall, then read it from JS via getAttribute, eliminating the
  nofilter usage that PS validator flags as a security risk.

views/templates/admin/field-option-settings/helpers/options/options.tpl
@@ -27,11 +27,11 @@
         <div class="col-lg-5">
             <input
                     type="password"
-                    {if isset($field['id'])} id="{$field['id']}"{/if}
-                    class="{if isset($field['class'])}{$field['class']}{/if}"
+                    {if isset($field['id'])} id="{$field['id']|escape:'htmlall':'UTF-8'}"{/if}
+                    class="{if isset($field['class'])}{$field['class']|escape:'htmlall':'UTF-8'}{/if}"
                     size="{if isset($field['size'])}{$field['size']|intval}{else}5{/if}"
-                    name="{$key}"
-                    value="{$field['value']}"
+                    name="{$key|escape:'htmlall':'UTF-8'}"
+                    value="{$field['value']|escape:'htmlall':'UTF-8'}"
                     {if isset($field['autocomplete']) && !$field['autocomplete']} autocomplete="off"{/if} />
         </div>
     {/if}

views/templates/front/saferpay_wait.tpl
@@ -20,6 +20,7 @@
  *@license   SIX Payment Services
  *}
 <h2>{l s='Awaiting payment status' mod='saferpayofficial'}</h2>
+<div id="saferpay-await-config" data-status-endpoint="{$checkStatusEndpoint|escape:'htmlall':'UTF-8'}"></div>
 <div class="saferpay-spinner">
     <div class="rect1"></div>
     <div class="rect2"></div>
@@ -90,8 +91,8 @@
     (function awaitSaferpayPaymentStatus() {
         var timeout = 3000;
         var request = new XMLHttpRequest();
-        // nofilter is needed for url with variables
-        request.open('GET', '{$checkStatusEndpoint|escape:'javascript':'UTF-8' nofilter}', true);
+        var endpoint = document.getElementById('saferpay-await-config').getAttribute('data-status-endpoint');
+        request.open('GET', endpoint, true);
 
         request.onload = function() {
             if (request.status >= 200 && request.status < 400) {

commit bdce477d2f269d3d8d6d8f0576658a77799f9ccd

SL-378 Fix API password corruption and plaintext exposure on settings save
The password_input value was HTML-escaped in the template (regression from
f7adabca) and pre-filled from the stored value. On save without re-typing,
the escaped value was submitted back and double-encoded, silently corrupting
passwords containing & < > " and causing 401 errors. The stored password was
also exposed in clear text in the page source.

- Stop pre-filling the password into the value attribute
- Keep the stored password when the field is submitted empty
- Add autocomplete off and a 'leave empty to keep' hint

controllers/admin/AdminSaferPayOfficialSettingsController.php
@@ -56,6 +56,9 @@ class AdminSaferPayOfficialSettingsController extends ModuleAdminController
 
     public function postProcess()
     {
+        $this->keepExistingPasswordWhenSubmittedEmpty(SaferPayConfig::PASSWORD);
+        $this->keepExistingPasswordWhenSubmittedEmpty(SaferPayConfig::PASSWORD . SaferPayConfig::TEST_SUFFIX);
+
         parent::postProcess();
 
         /** @var Configuration $configuration */
@@ -80,6 +83,15 @@ class AdminSaferPayOfficialSettingsController extends ModuleAdminController
         $this->validateTerminalId();
 
         return true;
+    }
+
+    private function keepExistingPasswordWhenSubmittedEmpty($key)
+    {
+        if (!isset($_POST[$key]) || $_POST[$key] !== '') {
+            return;
+        }
+
+        $_POST[$key] = \Configuration::get($key);
     }
 
     private function validateTerminalId()
@@ -489,7 +501,10 @@ class AdminSaferPayOfficialSettingsController extends ModuleAdminController
                     'title' => $this->module->l('JSON API Password'),
                     'type' => 'password_input',
                     'class' => 'fixed-width-xl',
-                    'value' => \Configuration::get(SaferPayConfig::PASSWORD . SaferPayConfig::TEST_SUFFIX),
+                    'autocomplete' => false,
+                    'placeholder' => \Configuration::get(SaferPayConfig::PASSWORD . SaferPayConfig::TEST_SUFFIX)
+                        ? $this->module->l('Leave empty to keep the saved password')
+                        : '',
                 ],
                 SaferPayConfig::CUSTOMER_ID . SaferPayConfig::TEST_SUFFIX => [
                     'title' => $this->module->l('Customer ID'),
@@ -557,7 +572,10 @@ class AdminSaferPayOfficialSettingsController extends ModuleAdminController
                     'title' => $this->module->l('JSON API Password'),
                     'type' => 'password_input',
                     'class' => 'fixed-width-xl',
-                    'value' => \Configuration::get(SaferPayConfig::PASSWORD),
+                    'autocomplete' => false,
+                    'placeholder' => \Configuration::get(SaferPayConfig::PASSWORD)
+                        ? $this->module->l('Leave empty to keep the saved password')
+                        : '',
                 ],
                 SaferPayConfig::CUSTOMER_ID => [
                     'title' => $this->module->l('Customer ID'),

views/templates/admin/field-option-settings/helpers/options/options.tpl
@@ -31,7 +31,8 @@
                     class="{if isset($field['class'])}{$field['class']|escape:'htmlall':'UTF-8'}{/if}"
                     size="{if isset($field['size'])}{$field['size']|intval}{else}5{/if}"
                     name="{$key|escape:'htmlall':'UTF-8'}"
-                    value="{$field['value']|escape:'htmlall':'UTF-8'}"
+                    value=""
+                    {if isset($field['placeholder']) && $field['placeholder']} placeholder="{$field['placeholder']|escape:'htmlall':'UTF-8'}"{/if}
                     {if isset($field['autocomplete']) && !$field['autocomplete']} autocomplete="off"{/if} />
         </div>
     {/if}